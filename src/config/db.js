09a6d4090193995913edb72bbb45c51274423bdb
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-23 12:43:42 -0500

    Add database connection setup with Prisma and dotenv

---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+const { PrismaClient } = require("@prisma/client/extension");
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const prisma = new PrismaClient({
+      log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['error']
+});
+
+const connectDB = async () => {
+    try {
+        await prisma.$connect();
+        console.log("DB Connection Success");
+    } catch (error) { 
+        console.log(`DB Connection error: ${error}`); 
+        process.exit(1);
+    }
+};
+
+const disconnectDB = async () => {
+    try {
+        await prisma.$disconnect();
+        console.log("DB Disconnect Success");
+    } catch (error) { 
+        console.log(`DB Disconnect error: ${error}`); 
+        process.exit(1);
+    }
+}
+
+export { connectDB, disconnectDB };
