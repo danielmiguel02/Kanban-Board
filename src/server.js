446c447a21e7fe87c8fe055c134a65fcb421a946
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-25 13:05:24 -0500

    Refactor server.js to properly import and use authRoute for authentication API

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB, disconnectDB } from './config/db.js';
 
 // Import routes
-
+import authRoute from './routes/authRoute.js';
 
 dotenv.config();
 connectDB();
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API Routes
-
+app.use('/auth', authRoute);
 
 const PORT = process.env.PORT || 9001;
 const server = app.listen(PORT, () => {
