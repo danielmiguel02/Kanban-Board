164cb0116e9071636166cba0ac8ff1b0d9d3e84e
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-28 13:16:41 -0500

    feat: implement cookie setup from utils/generateToken to authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { registerUserService, loginUserService } from '../services/authService.js';
+
+dotenv.config();
 
 const registerUser = async (req, res) => {
     try {
@@ -18,6 +21,13 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const result = loginUserService(req.body);
+
+        res.cookie("jwt", result.token, {
+            httpOnly: true,
+            secore: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
 
         return res.status(200).json({
             message: "User logged in successfully",
