3d822b259f08f142f3e92d4922f46d89cf3b3924
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-29 11:52:18 -0500

    feat: authMiddleware implemented to take care of token validation

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import { verifyToken } from '../utils/verifyToken.js';
+import { prisma } from '../config/db.js';
+
+export const authMiddleware = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization?.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies?.jwt) {
+        token = req.cookies.jwt;
+    }
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Not authorized, no token provided.",
+        });
+    }
+
+    try {
+        const decoded = verifyToken(token);
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id },
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "User no longer exists.",
+            });
+        }
+
+        req.user = user;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Not authorized, token failed",
+        });
+    }
+};
