e3ed56502322c732967f03d95ceed06e5da22502
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-30 13:21:31 -0500

    update: route to editBoard

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBoard } from '../controllers/boardController.js';
+import { createBoard, editBoard } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createBoard);
+router.patch("/", authMiddleware, editBoard);
 
 export default router;
