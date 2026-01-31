889d2350311f7426efcdd746e349978ed78e7c29
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-31 14:20:18 -0500

    update: added try and catch to handle error messages

--- src/controllers/columnController.js
+++ src/controllers/columnController.js
@@ -1,23 +1,29 @@
 import { createColumnService } from '../services/columnService.js';
 
 const createColumn = async (req, res) => {
-    const boardId = Number(req.params.boardId);
+    try {
+        const boardId = Number(req.params.boardId);
 
-    if (isNaN(boardId)) {
-        return res.status(400).json({
-            message: "Invalid board ID"
+        if (isNaN(boardId)) {
+            return res.status(400).json({
+                message: "Invalid board ID"
+            });
+        }
+
+        const result = await createColumnService({
+            data: req.body,
+            boardId: boardId
         });
-    }
 
-    const result = await createColumnService({
-        data: req.body,
-        boardId: boardId
-    });
-
-    return res.status(201).json({
-        message: "Column created successfully",
-        result
-    });
+        return res.status(201).json({
+            message: "Column created successfully",
+            result
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    };
 };
 
 export { createColumn };
