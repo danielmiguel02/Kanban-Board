ce394f315bdf20bbc98536ea85508dcc9ca549d7
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-31 13:46:44 -0500

    update: call getColumnsLastPos so we keep track of columns positions and assign the right pos to positions

--- src/services/columnService.js
+++ src/services/columnService.js
@@ -1,18 +1,19 @@
-import { getLastColumnPosition, createColumn } from "../repositories/columnRepository.js";
+import { getColumnsLastPos, createColumn } from "../repositories/columnRepository.js";
 
 const createColumnService = async (data) => {
     const { name, boardId } = data;
-
-    const columnPosition = getLastColumnPosition(boardId);
 
     if (!name) {
         throw new Error("Name is required to create a column.");
     }
 
+    const columnsLastPos = (await getColumnsLastPos(boardId)) ?? 0;
+
     const createdColumn = await createColumn({
         data: {
             name,
-            columnPosition,
+            position: columnsLastPos + 1,
+            boardId
         },
     });
 
@@ -21,7 +22,7 @@ const createColumnService = async (data) => {
             column: {
                 id: createdColumn.id,
                 name: createdColumn.name,
-                position: columnPosition,
+                position: columnsLastPos + 1,
                 boardId: boardId
             },
         },
