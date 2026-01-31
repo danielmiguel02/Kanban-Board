08c3e28f044ba25752086be458ca74057a5c3ec3
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-01-31 13:48:04 -0500

    fix: changed names from board to columns

--- src/controllers/columnController.js
+++ src/controllers/columnController.js
@@ -1,17 +1,17 @@
 import { createColumnService } from '../services/columnService.js';
 
 const createColumn = async (req, res) => {
-    const boardId = Number(req.params.boardId);
+    const columnId = Number(req.params.columnId);
 
-    if (isNaN(boardId)) {
+    if (isNaN(columnId)) {
         return res.status(400).json({
-            message: "Invalid board ID"
+            message: "Invalid column ID"
         });
     }
 
     const result = await createColumnService({
         data: req.body,
-        boardId: boardId
+        columnId: columnId
     });
 
     return res.status(201).json({
