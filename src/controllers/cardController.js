b4ee796838e6d1ce1592e0c26be4751b0ba92f6f
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-03 12:48:50 -0500

    feat: created and implemented card controller to create cards

---
+++ src/controllers/cardController.js
@@ -0,0 +1,30 @@
+import { createColumnService } from "../services/cardService.js";
+
+const createCard = async (req, res) => {
+    try {
+        const columnId = Number(req.params.columnId);
+
+        if (isNaN(columnId)) {
+            return res.status(400).json({
+                message: "Invalid column ID"
+            });
+        }
+
+        const result = await createCardService({
+            data: req.body,
+            columnId: columnId,
+            userId: req.user.id
+        });
+
+        return res.status(201).json({
+            message: "Card created successfully",
+            result
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export { createCard };
