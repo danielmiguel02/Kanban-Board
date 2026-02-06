63780cfb3cbc4a253c1d8e94408b239e79aa5e3d
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-06 12:29:40 -0500

    update: updated routes to take delete route

--- src/routes/cardRoute.js
+++ src/routes/cardRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createCard, editCard } from "../controllers/cardController.js";
+import { createCard, editCard, deleteCard } from "../controllers/cardController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/:columnId", authMiddleware, createCard);
 router.patch("/:cardId", authMiddleware, editCard);
+router.delete("/:cardId", authMiddleware, deleteCard);
 
 export default router;
