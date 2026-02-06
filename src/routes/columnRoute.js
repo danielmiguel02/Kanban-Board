27e4cd73ae40d2e5d4c12a5bceffd425389e1a1f
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-06 11:28:48 -0500

    update: updated routes to take delete route for columns

--- src/routes/columnRoute.js
+++ src/routes/columnRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createColumn, editColumn } from '../controllers/columnController.js';
+import { createColumn, editColumn, deleteColumn } from '../controllers/columnController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/:boardId", authMiddleware, createColumn);
 router.patch("/:columnId", authMiddleware, editColumn);
+router.delete("/:columnId", authMiddleware, deleteColumn);
 
 export default router;
