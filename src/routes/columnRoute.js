1f7773b38e56276b14956c6223cd32835fa92b2b
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-10 12:35:01 -0500

    update: update routes to take archive route

--- src/routes/columnRoute.js
+++ src/routes/columnRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createColumn, editColumn, deleteColumn } from '../controllers/columnController.js';
+import { createColumn, editColumn, deleteColumn, archiveColumn } from '../controllers/columnController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/:boardId", authMiddleware, createColumn);
 router.patch("/:columnId", authMiddleware, editColumn);
 router.delete("/:columnId", authMiddleware, deleteColumn);
+router.patch("/:columnId/archive", authMiddleware, archiveColumn);
 
 export default router;
