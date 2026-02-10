78c1ab49daf57b39b05a992da0df8bc5210a9f02
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-10 12:53:17 -0500

    update: updated board routes to take archive route

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBoard, editBoard, deleteBoard } from '../controllers/boardController.js';
+import { createBoard, editBoard, deleteBoard, archiveBoard } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/", authMiddleware, createBoard);
 router.patch("/:id", authMiddleware, editBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
+router.patch("/:boardId/archive", authMiddleware, archiveBoard);
 
 export default router;
