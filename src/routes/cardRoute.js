3cee9a95f9f2c24e6b7712412668c3611b91f9be
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-10 12:02:24 -0500

    fix: fixed patch routes so they don't conflit with one another

--- src/routes/cardRoute.js
+++ src/routes/cardRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/:columnId", authMiddleware, createCard);
 router.patch("/:cardId", authMiddleware, editCard);
 router.delete("/:cardId", authMiddleware, deleteCard);
-router.patch("/:cardId/:columnId", authMiddleware, moveCardToColumn);
-router.patch("/archive/:cardId", authMiddleware, archiveCard);
+router.patch("/:cardId/move/:columnId", authMiddleware, moveCardToColumn);
+router.patch("/:cardId/archive", authMiddleware, archiveCard);
 
 export default router;
