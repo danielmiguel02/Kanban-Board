3fed29413f21becc30f4767bb81371a6a62a4b8f
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-11 14:22:40 -0500

    update: updated routes to take unarchiveBoard route

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBoard, editBoard, deleteBoard, archiveBoard } from '../controllers/boardController.js';
+import { createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/", authMiddleware, createBoard);
 router.patch("/:id", authMiddleware, editBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
 router.patch("/:boardId/archive", authMiddleware, archiveBoard);
+router.patch("/:boardId/unarchive", authMiddleware, unarchiveBoard);
 
 export default router;
