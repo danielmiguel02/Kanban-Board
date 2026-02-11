23fb1755460b2674bb87117701d4b2f99def89d3
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-11 13:09:23 -0500

    fix: spelling fixed

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -138,15 +138,9 @@ const unarchiveCardService = async ({cardId, userId}) => {
         throw new Error("Card is not archived, can't unarchive");
     }
 
-<<<<<<< HEAD
     const columnArchived  = await isColumnArchived(card.columnId);
 
     if (columnArchived ) {
-=======
-    const columnArchived = await isColumnArchived(card.columnId);
-
-    if (columnArchived) {
->>>>>>> feat/kanban-repository
         throw new Error("Card column is archived, can't unarchive");
     }
 
