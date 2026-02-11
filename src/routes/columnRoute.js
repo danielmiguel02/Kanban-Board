45df91fad1dc87515cd06ca8a1e943981cd55c1b
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-11 13:11:14 -0500

    update: updated routes to take unarchiveColumn route

--- src/routes/columnRoute.js
+++ src/routes/columnRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createColumn, editColumn, deleteColumn, archiveColumn } from '../controllers/columnController.js';
+import { createColumn, editColumn, deleteColumn, archiveColumn, unarchiveColumn } from '../controllers/columnController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/:boardId", authMiddleware, createColumn);
 router.patch("/:columnId", authMiddleware, editColumn);
 router.delete("/:columnId", authMiddleware, deleteColumn);
 router.patch("/:columnId/archive", authMiddleware, archiveColumn);
+router.patch("/:columnId/unarchive", authMiddleware, unarchiveColumn);
 
 export default router;
