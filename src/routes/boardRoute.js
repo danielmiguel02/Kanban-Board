7cb4e3c3b7d9bf75fff0815219eb854a704a5c29
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-13 13:29:01 -0500

    update: updated board routes to take addMembersToBoard route

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard } from '../controllers/boardController.js';
+import { createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.patch("/:id", authMiddleware, editBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
 router.patch("/:boardId/archive", authMiddleware, archiveBoard);
 router.patch("/:boardId/unarchive", authMiddleware, unarchiveBoard);
+
+router.post("/:boardId/members", authMiddleware, addMembersToBoard);
 
 export default router;
