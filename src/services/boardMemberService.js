7bfedb9f47cc9aa1aa8443a7b8b2460dad18c849
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-13 13:12:05 -0500

    feat: implemented addMembersToBoardService for validation and passing data to createBoardMember rep

---
+++ src/services/boardMemberService.js
@@ -0,0 +1,55 @@
+import { findBoardById } from "../repositories/boardRepository.js";
+import { findUserByEmail } from "../repositories/userRepository.js";
+import { findBoardMember, createBoardMember } from "../repositories/boardMemberRepository.js";
+
+
+const addMembersToBoardService = async ({data, boardId, userId}) => {
+    const { email, role } = data;
+
+    if (!email) {
+        throw new Error("Email is required to add member");
+    }
+
+    if (!["VIEW", "EDIT"].includes(role)) {
+        throw new Error("Invalid role");
+    }
+
+    const board = await findBoardById(boardId);
+
+    if (!board) {
+        throw new Error("Board not found");
+    }
+
+    if (board.ownerId !== userId) {
+        throw new Error("Not authorized to add members");
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+        throw new Error("User not found by that email");
+    }
+
+    const existingMember = await findBoardMember(boardId, user.id);
+
+    if (existingMember) {
+        throw new Error("User is already a member of this board");
+    }
+
+    const boardMember = await createBoardMember({
+        boardId,
+        userId: user.id,
+        role,
+    });
+
+    return {
+        boardMember: {
+            id: boardMember.id,
+            boardId: boardMember.boardId,
+            userId: boardMember.userId,
+            role: boardMember.role,
+        },
+    };
+};
+
+export { addMembersToBoardService };
