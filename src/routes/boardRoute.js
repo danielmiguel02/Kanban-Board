90123becfe6995ea172a71d5a59862b7d27dd2a3
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-07-06 12:59:34 -0400

    update: updated boardRoute to take both getBoards and getBoard routes

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard, removeMembersFromBoard, editMembersRoleFromBoard } from '../controllers/boardController.js';
+import { getBoard, getBoards, createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard, removeMembersFromBoard, editMembersRoleFromBoard } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.get("/", authMiddleware, getBoards);
+router.get("/:boardId", authMiddleware, getBoard);
 router.post("/", authMiddleware, createBoard);
 router.patch("/:id", authMiddleware, editBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
