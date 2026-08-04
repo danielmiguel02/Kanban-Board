2180fcb458732e1fbd87a958104829aa3f9dc06d
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-08-04 13:11:16 -0400

    update: updated boardRoute to have archived boards route

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -1,18 +1,12 @@
 import express from 'express';
-<<<<<<< HEAD
-import { getBoard, getBoards, createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard, removeMembersFromBoard, editMembersRoleFromBoard } from '../controllers/boardController.js';
-=======
-import { getBoards, createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard, removeMembersFromBoard, editMembersRoleFromBoard } from '../controllers/boardController.js';
->>>>>>> feat/kanban-controller
+import { getBoard, getBoards, createBoard, editBoard, deleteBoard, archiveBoard, unarchiveBoard, addMembersToBoard, removeMembersFromBoard, editMembersRoleFromBoard, getArchivedBoards } from '../controllers/boardController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getBoards);
-<<<<<<< HEAD
 router.get("/:boardId", authMiddleware, getBoard);
-=======
->>>>>>> feat/kanban-controller
+router.get("/archived", authMiddleware, getArchivedBoards);
 router.post("/", authMiddleware, createBoard);
 router.patch("/:id", authMiddleware, editBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
